443fca440082dbb30a8c4e7c594aa41edfa486dd
jannat710 <jannatul0040@gmail.com>
2025-05-11 20:47:53 +0600

    Added task-1

---
+++ task-1.ts
@@ -0,0 +1,21 @@
+type Person = {
+  name: string;
+  age: number;
+  gender: "male" | "female";
+};
+
+const person: Person[] = [
+  { name: "Jannat", age: 24, gender: "female" },
+  { name: "Jemi", age: 22, gender: "female" },
+  { name: "Jyoti", age: 33, gender: "female" },
+  { name: "Evan", age: 28, gender: "male" },
+  { name: "Efti", age: 27, gender: "male" },
+];
+
+function getMaleNames(person: Person[]): string[] {
+  return person
+    .filter((person) => person.gender === "male")
+    .map((person) => person.name);
+}
+
+const maleNames = getMaleNames(person);
