7c7b6dab5d16fc646be9ea56b6c7744c950fcc61
jannat710 <jannatul0040@gmail.com>
2025-05-11 20:59:00 +0600

    Added task-2

---
+++ task-2.ts
@@ -0,0 +1,19 @@
+type Book = {
+  title: string;
+  author: string;
+  year: number;
+};
+
+const books: Book[] = [
+  { title: "JavaScript for Beginners", author: "Jhankar Mahbub", year: 2021 },
+  { title: "React Essentials", author: "Programming Hero Team", year: 2022 },
+  { title: "Mastering Full-Stack", author: "PH Engineering Team", year: 2023 },
+  { title: "Clean Code", author: "Robert C. Martin", year: 2008 },
+  { title: "TypeScript Handbook", author: "Microsoft", year: 2020 },
+];
+
+function getBookTitles(books: Book[]): string[] {
+  return books.map((book) => book.title);
+}
+
+const titles = getBookTitles(books);
